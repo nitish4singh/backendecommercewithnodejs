83bd249a5b068fe58166ed4018f28d369891e0b9
nitish4singh <nitishsingh4748@gmail.com>
2023-05-18 15:06:22 +0545

    applying jwt token, user curd operation

--- controller/userController.js
+++ controller/userController.js
@@ -1,6 +1,8 @@
-const  User =require("../models/userModel")
+const { generateToken } = require("../config/jwtToken");
+const  User = require("../models/userModel")
 const asyncHandler =require("express-async-handler");
 
+// register the user constroller
 
 const createUser =asyncHandler(
     async(req,res)=>{
@@ -17,6 +19,83 @@ const createUser =asyncHandler(
        }
        }
 );
+// login controller
+
+const loginUserCtrl =asyncHandler(async (req,res)=>{
+const {email,password}=req.body;
+console.log(email,password);
+
+// check the user is exit or not
+const findUser =await User.findOne({email});
+if(findUser && await findUser.isPasswordMatched(password)){
+res.json({
+_id: findUser?._id,
+firstname: findUser?.firstname,
+lastname: findUser?.lastname,
+email: findUser?.email,
+mobile: findUser?.mobile,
+token: generateToken(findUser?._id),
+});
+}else{
+    throw new Error('Invalid Credentials');
+}
+
+});
+
+//Get all user 
+const getallUser =asyncHandler(async(req,res)=>{
+   try{
+    const getUsers =await User.find();
+    res.json(getUsers);
+   }catch(error){
+throw new Error (error);
+   }
+})
+// get a single user 
+const getsingleUser =asyncHandler(async(req,res)=>{
+    const{id} =req.params;
+    try{
+     const getsingleUser =await User.findById(id);
+     res.json(getsingleUser);
+    }catch(error){
+ throw new Error (error);
+    }
+ });
+
+ // delete a user 
+const deleteaUser =asyncHandler(async(req,res)=>{
+    const{id} =req.params;
+    try{
+     const deleteaUser =await User.findByIdAndDelete(id);
+     res.json(deleteaUser);
+    }catch(error){
+ throw new Error (error);
+    }
+ });
+//update a user 
+
+const updateauser =asyncHandler(async(req,res)=>{
+    const{id} =req.params;
+    try{
+     const updateauser =await User.findByIdAndUpdate(id,{
+        firstname:req?.body?.firstname,
+        lastname:req?.body?.lastname,
+        email:req?.body?.email,
+        mobile:req?.body?.mobile
+     },
+     {
+        new:true,
+     }
+     );
+     res.json(updateauser);
+    }catch(error){
+ throw new Error (error);
+    }
+ });
 
 
-module.exports ={createUser};
+
+
+
+
+module.exports ={createUser,loginUserCtrl, getallUser,getsingleUser,deleteaUser,updateauser};
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,17 @@
+
+
 const express = require('express');
-const { createUser } = require('../controller/userController');
+const { createUser, loginUserCtrl, getallUser, getsingleUser, deleteaUser, updateauser } = require('../controller/userController');
 const router =express.Router();
 
 
 router.post('/register',createUser);
+router.post('/login',loginUserCtrl);
+router.get('/all-users',getallUser);
+router.get('/:id',getsingleUser);
+router.delete('/:id',deleteaUser);
+router.put('/:id',updateauser);
+
+
 
 module.exports =router;
