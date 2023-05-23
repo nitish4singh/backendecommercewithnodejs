3bf8ae123e54dff36a5c6142d3b8d17e1f01cadf
nitish4singh <nitishsingh4748@gmail.com>
2023-05-23 15:13:51 +0545

    rating function added

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,11 +1,13 @@
 const express =require("express");
-const { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct, addToWishlist } = require("../controller/productController");
+const { createProduct, getaProduct, getAllProduct, updateProduct, deleteProduct, addToWishlist, rating } = require("../controller/productController");
 const {isAdmin,authMiddleware} = require("../middlewares/authMiddleware");
 const router =express.Router();
 
 
 router.post("/" ,createProduct);
-router.put('/wishlist',authMiddleware,addToWishlist)
+router.put('/wishlist',authMiddleware,addToWishlist);
+router.put('/rating',authMiddleware,rating);
+
 router.put("/:id" ,authMiddleware,isAdmin,updateProduct);
 router.delete("/:id",authMiddleware,isAdmin ,deleteProduct);
 router.get("/" , getAllProduct);
